5474f30749e88ce2a1804e43fd8e32894a8bfa8d
Eao-Kind <a2393754@gmail.com>
2020-06-05 15:39:38 +0800

    多线程求和1到100

--- src/chapter_1/SY14.java
+++ src/chapter_1/SY14.java
@@ -1,21 +1,29 @@
 package chapter_1;
 
 public class SY14 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("方式一:实现Runnable接口");
-		for (int i = 0; i < 10; i++) 
-			new Thread(new MyAddOne(i * 10 + 1, i * 10 + 10)).start();
-		Thread.currentThread().setPriority(3);
+		Thread t1 = new Thread();
+		for (int i = 0; i < 10; i++) {
+			t1 = new Thread(new MyAddOne(i * 10 + 1, i * 10 + 10));
+			t1.start();
+		}
+		t1.join();
+		// new Thread(new MyAddOne(i * 10 + 1, i * 10 + 10)).start();}
+		Thread.sleep(50);
+		System.out.println("总和" + MyAddOne.allSum);
 		System.out.println("\n方式二:继承Thread类");
-		for (int i = 0; i < 10; i++) 
-			new Thread(new MyAddTwo(i * 10 + 1, i * 10 + 10)).start();
-		Thread.currentThread().setPriority(5);
+		for (int i = 0; i < 10; i++)
+			new MyAddTwo(i * 10 + 1, i * 10 + 10).start();
+		Thread.sleep(50);
+		System.out.println("总和" + MyAddTwo.allSum);
 	}
 }
 
 class MyAddOne implements Runnable { // 方式一 实现Runnable类
-	int start, end, sum;
-	
+	int start, end, temp;
+	static int allSum;
+
 	MyAddOne(int start, int end) {
 		this.start = start;
 		this.end = end;
@@ -23,15 +31,17 @@ class MyAddOne implements Runnable { // 方式一 实现Runnable类
 
 	public void run() {
 		for (int i = start; i <= end; i++) {
-			sum += i;
+			temp += i;
 		}
-		System.out.print(sum + " ");
+		System.out.println(Thread.currentThread().getName() + "  " + temp);
+		allSum += temp;
 	}
 }
 
 class MyAddTwo extends Thread { // 方式二 继承Thread类
-	int start, end, sum;
-	
+	int start, end, temp;
+	static int allSum;
+
 	MyAddTwo(int start, int end) {
 		this.start = start;
 		this.end = end;
@@ -39,8 +49,9 @@ class MyAddTwo extends Thread { // 方式二 继承Thread类
 
 	public void run() {
 		for (int i = start; i <= end; i++) {
-			sum += i;
+			temp += i;
 		}
-		System.out.print(sum + " ");
+		System.out.println(Thread.currentThread().getName() + "  " + temp);
+		allSum += temp;
 	}
 }
