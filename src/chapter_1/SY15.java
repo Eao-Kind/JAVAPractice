ef92ce3d606afd45f5fe48d4368e886d7f20f78a
Eao-Kind <a2393754@gmail.com>
2020-06-12 14:25:09 +0800

    泛型和LinkedList的使用

--- src/chapter_1/SY15.java
+++ src/chapter_1/SY15.java
@@ -9,18 +9,15 @@ public class SY15 {
 		String[] bookName = { "Java2SE", "JavaEE", "JavaWeb", "数据结构", "C++程序设计", "Java2ME", "操作系统" };
 		Double[] bookPrice = { 29.0, 21.0, 22.0, 29.0, 34.0, 32.0, 29.0 };
 		List<Book> bookList = new LinkedList<Book>();
-		for (int i = 0; i < bookPrice.length; i++) {
+		for (int i = 0; i < bookPrice.length; i++) 
 			bookList.add(new Book(bookName[i], bookPrice[i]));
-		}
 		System.out.println("排序前：链表中的数据");
-		for (Book b : bookList) {
+		for (Book b : bookList) 
 			System.out.println(b.name + "---" + b.price);
-		}
 		Collections.sort(bookList);
 		System.out.println("\n排序后：链表中的数据");
-		for (Book b : bookList) {
+		for (Book b : bookList) 
 			System.out.println(b.name + "---" + b.price);
-		}
 		System.out.println();
 		Book js = new Book("JavaScript", 29.0);
 		int index = 0;
