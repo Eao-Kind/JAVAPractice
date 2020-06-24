c28e8ada6f9d42ed39088639efd7afe6efa601ec
Eao-Kind <a2393754@gmail.com>
2020-06-24 21:07:02 +0800

    词频统计 优化设计

--- src/project/project2.java
+++ src/project/project2.java
@@ -3,8 +3,7 @@ package project;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -17,17 +16,16 @@ public class project2 {
 	public static void main(String[] args) throws FileNotFoundException {
 		TextTool tl = new TextTool();
 		ArrayList<String> wordList = tl.getAllWord(new File("test.txt"), "[A-Za-z]+"); // 获取单词列表 正则表达式
-		System.out.println("一共出现的单词数：" + wordList.size());
+		System.out.println("\n一共出现的单词数：" + wordList.size());
 		Map<String, Integer> mp = tl.wordStatistics(wordList); // 按字典顺序排序
-		System.out.println("互不相同的单词数：" + mp.size());
-		System.out.println("按字典顺序：");
-		
-		for(String key : mp.keySet()) {
+		System.out.println("互不相同的单词数：" + mp.size() +"\n\n按字典顺序：");
+
+		for (String key : mp.keySet()) {
 			System.out.println("单词个数：" + mp.get(key) + " 单词为：" + key);
 		}
 
 		List<Entry<String, Integer>> list = tl.mySort(mp); // 按照词频顺序排序
-		System.out.println("按词频顺序：");
+		System.out.println("\n按词频顺序：");
 		for (Map.Entry<String, Integer> entry : list) {
 			System.out.println("单词个数：" + entry.getValue() + " 单词为：" + entry.getKey());
 		}
@@ -54,7 +52,7 @@ class TextTool {
 	public Map<String, Integer> wordStatistics(ArrayList<String> temp) { // 词频统计 默认按照字典排序
 		Map<String, Integer> mp = new TreeMap<String, Integer>(); // 键值对
 		for (String st : temp) {
-			if (mp.get(st) != null)
+			if (mp.containsKey(st))
 				mp.put(st, mp.get(st) + 1); // 词频统计+1
 			else
 				mp.put(st, 1);
@@ -63,12 +61,8 @@ class TextTool {
 	}
 
 	public List<Entry<String, Integer>> mySort(Map<String, Integer> temp) { // Map 按Value排序
-		List<Entry<String, Integer>> list = new ArrayList<Entry<String, Integer>>(temp.entrySet());
-		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2) {
-				return o2.getValue().compareTo(o1.getValue()); // 按值排序
-			}
-		});
+		List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(temp.entrySet());
+		list.sort(Map.Entry.comparingByValue());
 		return list;
 	}
 }
